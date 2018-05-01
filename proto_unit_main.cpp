a2c937b670c2a16d0a6d2e17a0e6106af548d233
will2055 <brandon.w2055@gmail.com>
2018-05-01 19:41:15 +0000

    new source

--- proto_unit_main.cpp
+++ proto_unit_main.cpp
@@ -6,7 +6,9 @@
 #include <iostream>
 #include <unistd.h>
 #include <cstdint>
-//#include <wiringPiI2C.h>
+#include <wiringPi.h>
+#include <wiringSerial.h>
+#include <wiringPiI2C.h>
 
 using namespace std;
 
@@ -15,7 +17,7 @@ class CarbonDioxide{
 		const uint8_t dev_id = 0x70;
 		const uint8_t dev_addr = 0b000001100;
 		const uint8_t dev_data = 0x00;
-		uint8_t recieve_frame[7];
+		uint8_t sensor_data[7];
 		uint8_t crc;
 		void initCRC(){
 			uint8_t byte_sum = 0x00;
@@ -27,15 +29,15 @@ class CarbonDioxide{
 		uint8_t send_frame[6]={dev_addr,dev_data,dev_data,
 			dev_data,dev_data,crc};
 	public:
-		int mics_fd;
-		int voc, carbon_dioxide
+		int mics_fd,read_res;
+		int voc, carbon_dioxide;
 		int openComm(){
 			if((mics_fd=wiringPiI2CSetup(0x70)) < 0){
 				//cout<<"Error Occured in setting up Dev_ID: 0x70"<<endl;
 				return 1;
 			}
 			else{
-				if((read_res = read(fd, sensor_data, 8)) != 8){
+				if((read_res = read(mics_fd, sensor_data, 8)) != 8){
 					cout<<"There could be an issue reading from the file."<<endl;
 					cout<<"Read returned: "<<read_res<<endl;
 					return 2;
@@ -68,7 +70,7 @@ class Linx_GPS{
 		int setup;
 		int baud = 9600;
 	public:
-		void openGPS_Serial(){
+		int openGPS_Serial(){
 			if((device = serialOpen("/dev/ttyS0", baud)) < 0){
 				//cout<<"The serial device couldn't be opened."<<endl;
 				return 1;
@@ -84,7 +86,7 @@ class Linx_GPS{
 						for(int i=1; i<100; i++){
 							char next_value;
 							next_value = serialGetchar(device);
-							if(next_value == '\n'{
+							if(next_value == '\n'){
 								exit = true;
 								break;
 							}
@@ -108,7 +110,7 @@ class Particulate{
 		int setup;
 		int baud = 57600;
 	private:
-		void openPM_Serial(){
+		int openPM_Serial(){
 			if((device = serialOpen("/dev/ttyUSB0", baud)) < 0){
 				return 1;
 			}
@@ -172,7 +174,6 @@ int main(){
 			cout<<"Error occured in reading from the I2C device"<<endl;
 		}
 		
-		cout<<endl;
 		cout<<endl;
 	}
 	return 0;
